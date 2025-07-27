6d0d24dc5c61c698915809f50afc4deacc0ab64e
Sasha Lapommeray <sashalapommeray@proton.me>
2025-07-27 16:11:15 -0400

    fixes

--- save.py
+++ save.py
@@ -1,25 +1,3 @@
-# import boto3
-# from pprint import pprint
-
-
-# table_name = "visitor_counter"                                       
-# dynamodb_resource = boto3.resource("dynamodb")
-# users_table = dynamodb_resource.Table(table_name)
-
-# def lambda_handler(event,context):
-#     response = users_table.update_item(
-#         Key={"id": "counter"},
-#         UpdateExpression="set visitor_number = visitor_number + :n",
-#         ExpressionAttributeValues={
-#             ":n": 1,        },
-#         ReturnValues="UPDATED_NEW",
-#     )
-#     #pprint(response["Attributes"])
-#     return response["Attributes"]["visitor_number"]
-
-
-
-
 import boto3
 import json
 
@@ -28,19 +6,7 @@ dynamodb_resource = boto3.resource("dynamodb")
 users_table = dynamodb_resource.Table(table_name)
 
 def lambda_handler(event, context):
-    # Handle preflight OPTIONS request
-    if event.get("httpMethod") == "OPTIONS":
-        return {
-            "statusCode": 200,
-            "headers": {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Headers": "Content-Type",
-                "Access-Control-Allow-Methods": "GET,POST,OPTIONS"
-            },
-            "body": ""
-        }
 
-    # Update DynamoDB counter
     response = users_table.update_item(
         Key={"id": "counter"},
         UpdateExpression="set visitor_number = visitor_number + :n",
@@ -49,22 +15,6 @@ def lambda_handler(event, context):
     )
 
     # Convert Decimal to int for JSON serialization
-    visitor_count = int(response["Attributes"]["visitor_number"])
+    visitor_count = response["Attributes"]["visitor_number"]
 
-    # Return JSON response with CORS headers
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Access-Control-Allow-Origin": "*",
-        },
-        "body": json.dumps({
-            "visitor_number": visitor_count
-        })
-    }
-
-
-
-    # it reruns every time someone reloads. any way to keep track of individual visitors and not just increment per load? cookies?
-    # yes, but probably not here. in javascript, only run the api if there is a new user
-    # but if someone finds the link and keeps reloading it they could fuck up the visitor count
-    # keep it restricted to iam user? would it still work?
+    return visitor_count
